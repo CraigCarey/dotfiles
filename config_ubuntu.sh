985cad1f9770acb734519fd978ac55c20ea9a479
Craig <sgccarey@gmail.com>
2017-10-07 18:31:35 +0100

    Added vmshare folder setup

--- config_ubuntu.sh
+++ config_ubuntu.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Insert Guest additions CD
+# sudo ./VBoxLinuxAdditions-x86.run
+# restart
+
 sudo apt update
 sudo apt -y dist-upgrade
 
@@ -18,13 +22,14 @@ sudo apt --yes --force-yes install \
 							feh \
 							sshfs
 
-# sudo apt --yes --force-yes autoremove
-
 readonly HOME_DIR="/home/${USER}/"
 
 if [[ $(sudo dmidecode -s system-product-name) =~ "VirtualBox" ]]; then
 	sudo apt --yes --force-yes install virtualbox-guest-additions-iso
 	sudo adduser "$USER" vboxusers
+	sudo adduser "$USER" vboxsf
+	mkdir "/home/user/${USER}/share/"
+	sudo mount -t vboxsf -o uid=1000,gid=1000 vmshare "/home/user/${USER}/share/"
 fi
 
 readonly SUBL_DIR="/opt/sublime_text_3/"
@@ -38,7 +43,7 @@ fi
 readonly PYCHARM_DIR="/opt/pycharm-2017.2.3/"
 if [ ! -d "$PYCHARM_DIR" ]; then
 	wget "https://download.jetbrains.com/python/pycharm-professional-2017.2.3.tar.gz"
-	tar xf "pycharm-professional-2017.2.3.tar.gz" -C "/opt/"
+	sudo tar xf "pycharm-professional-2017.2.3.tar.gz" -C "/opt/"
 	sudo ln -s "${PYCHARM_DIR}bin/pycharm.sh" "/usr/bin/pyc"
 fi
 
