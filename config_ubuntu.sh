bcdb9acb41f66e4cb6f1a99e2ad388bcc5af1d52
Craig <sgccarey@gmail.com>
2017-11-05 20:33:42 +0000

    Added webstorm

--- config_ubuntu.sh
+++ config_ubuntu.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 # Insert Guest additions CD
-# sudo ./VBoxLinuxAdditions-x86.run
+# sudo ./VBoxLinuxAdditions.run
 # restart
 
 sudo apt update
 sudo apt -y dist-upgrade
 
 sudo apt --yes --force-yes install \
-							git \
-							build-essential \
-							radare2 \
-							python3.6 \
-							python3-venv \
-							python-pip \
-							i3 \
-							tree \
-							chromium-browser \
-							meld \
-							bless \
-							feh \
-							sshfs
+                           git \
+                           build-essential \
+                           radare2 \
+                           python3.6 \
+                           python3-venv \
+                           python-pip \
+                           i3 \
+                           tree \
+                           chromium-browser \
+                           meld \
+                           bless \
+                           feh \
+                           sshfs \
+                           ntpdate
 
 readonly HOME_DIR="/home/${USER}/"
 
@@ -28,8 +29,8 @@ if [[ $(sudo dmidecode -s system-product-name) =~ "VirtualBox" ]]; then
 	sudo apt --yes --force-yes install virtualbox-guest-additions-iso
 	sudo adduser "$USER" vboxusers
 	sudo adduser "$USER" vboxsf
-	mkdir "/home/user/${USER}/share/"
-	sudo mount -t vboxsf -o uid=1000,gid=1000 vmshare "/home/user/${USER}/share/"
+	mkdir "/home/${USER}/share/"
+	sudo mount -t vboxsf -o uid=1000,gid=1000 vmshare "/home/${USER}/share/"
 fi
 
 readonly SUBL_DIR="/opt/sublime_text_3/"
@@ -40,21 +41,36 @@ if [ ! -d "$SUBL_DIR" ]; then
 	sudo ln -s "${SUBL_DIR}sublime_text" "/usr/bin/sb"
 fi
 
-readonly PYCHARM_DIR="/opt/pycharm-2017.2.3/"
+readonly PYCHARM_DIR="/opt/pycharm/"
 if [ ! -d "$PYCHARM_DIR" ]; then
-	wget "https://download.jetbrains.com/python/pycharm-professional-2017.2.3.tar.gz"
-	sudo tar xf "pycharm-professional-2017.2.3.tar.gz" -C "/opt/"
-	sudo ln -s "${PYCHARM_DIR}bin/pycharm.sh" "/usr/bin/pyc"
+	readonly PYCHARM_DL="pycharm-professional-2017.2.4.tar.gz"
+	wget "https://download.jetbrains.com/python/${PYCHARM_DL}"
+	sudo mkdir "$PYCHARM_DIR"
+	sudo tar -xzf "$PYCHARM_DL" -C "$PYCHARM_DIR" --strip-components=1
+	sudo ln -s "${PYCHARM_DIR}bin/pycharm.sh" "/usr/bin/pycharm"
+fi
+
+readonly WEBSTORM_DIR="/opt/webstorm/"
+if [ ! -d "$WEBSTORM_DIR" ]; then
+	readonly WEBSTORM_DL="WebStorm-2017.2.5.tar.gz"
+	wget "https://download.jetbrains.com/webstorm/${WEBSTORM_DL}"
+	sudo mkdir "$WEBSTORM_DIR"
+	sudo tar -xzf "$WEBSTORM_DL" -C "$WEBSTORM_DIR" --strip-components=1
+	sudo ln -s "${WEBSTORM_DIR}bin/webstorm.sh" "/usr/bin/webstorm"
 fi
 
 cp "ubuntu_bashrc" "${HOME_DIR}.bashrc"
 cp "bash_aliases" "${HOME_DIR}.bash_aliases"
 cp "gitconfig" "${HOME_DIR}.gitconfig"
+cp "elcapitan2hr.jpg" "$HOME_DIR"
 
-feh --bg-scale "elcapitan2hr.jpg"
+feh --bg-scale "${HOME_DIR}elcapitan2hr.jpg"
 
 rm -rf "${HOME_DIR}Documents" "${HOME_DIR}Music" "${HOME_DIR}examples.desktop" "${HOME_DIR}Pictures" \
        "${HOME_DIR}Public" "${HOME_DIR}Templates" "${HOME_DIR}Videos" "${HOME_DIR}Firefox_wallpaper.png"
 
 # remap § key to #
 xmodmap -e "keycode 49 = numbersign"
+
+# update time
+sudo ntpdate time.nist.gov
