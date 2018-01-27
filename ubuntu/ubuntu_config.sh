cb7f8e5c6b7be600b47757a08b16b7b358f340dc
Craig <sgccarey@gmail.com>
2018-01-27 20:46:53 +0000

    Added Spotify

--- ubuntu/ubuntu_config.sh
+++ ubuntu/ubuntu_config.sh
@@ -69,6 +69,13 @@ if [ ! -d "$CLION_DIR" ]; then
 	sudo ln -s "${CLION_DIR}bin/clion.sh" "/usr/bin/clion"
 fi
 
+if [[ $(sudo dmidecode -s system-product-name) !~ "VirtualBox" ]]; then
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt-get update
+	sudo apt-get install spotify-client
+fi
+
 cp "bashrc" "${HOME_DIR}.bashrc"
 sudo cp "../hosts" "/etc/hosts"
 cp "../bash_aliases" "${HOME_DIR}.bash_aliases"
