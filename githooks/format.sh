5d8bbe47da4a2460d9de283bedaa8e54020649a8
Craig <sgccarey@gmail.com>
2019-02-07 09:22:36 +0000

    Fixed clang-format scripts

--- githooks/format.sh
+++ githooks/format.sh
@@ -1,29 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -eu
 
-readonly CLANG_FORMAT="$(which clang-format)"
+readonly CLANG_FORMAT=clang-format
 
 if [[ ! -x "$CLANG_FORMAT" ]]; then
-	printf "$CLANG_FORMAT is not found:\n"
-	printf "\t$ sudo apt-get install $CLANG_FORMAT\n"
+	printf "clang-format is not found:\n"
+	printf "\t$ sudo apt-get install clang-format\n"
 	exit 1
 fi
 
 readonly REPO_DIR="$(dirname "$(readlink -f "$0")")"
 pushd "$REPO_DIR/.." 1>/dev/null 2>&1
 
-for file in $(find . -iname "*.cpp" -type f); do
+for file in $(find . -iname "*.cpp"); do
 	printf "formatting: $file\n"
 	"$CLANG_FORMAT" -i "$file"
 done
 
-for file in $(find . -iname "*.hpp" -type f); do
+for file in $(find . -iname "*.hpp"); do
 	printf "formatting: $file\n"
 	"$CLANG_FORMAT" -i "$file"
 done
 
-for file in $(find . -iname "*.h" -type f); do
+for file in $(find . -iname "*.h"); do
 	printf "formatting: $file\n"
 	"$CLANG_FORMAT" -i "$file"
 done
