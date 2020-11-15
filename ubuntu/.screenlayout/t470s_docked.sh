6a509664bf4d86702fff1a11c62f3112ec61f9c0
Craig <sgccarey@gmail.com>
2020-11-15 13:31:14 +0000

    ubuntu: regolith install

--- ubuntu/.screenlayout/t470s_docked.sh
+++ ubuntu/.screenlayout/t470s_docked.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 # t470s
-
-readonly DISPLAY="DP2-2"
-
-if xrandr | grep -q "$disp connected "; then
-    xrandr --output "$disp" --primary
+if xrandr | grep -q "DP2-2 connected"; then
+	xrandr --output DP2-2 --primary
 	xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
 	xrandr --output DP2-1 --mode 1920x1080 --pos 4480x0 --rotate normal
-	xrandr --output DP2-2 --primary --mode 2560x1440 --pos 1920x0 --rotate normal
+	xrandr --output DP2-2 --mode 2560x1440 --pos 1920x0 --rotate normal
 fi
--- ubuntu/ubuntu_setup.sh
+++ ubuntu/ubuntu_setup.sh
@@ -15,106 +15,117 @@ pushd "$SCRIPT_DIR" > /dev/null
 sudo apt update && sudo apt -y upgrade
 
 sudo apt --yes --force-yes install \
-                           git \
-                           build-essential \
-                           cmake \
-                           meld \
-                           valgrind \
-                           tree \
-                           bless \
-                           openssh-server \
-                           sshfs \
-                           net-tools \
-                           tmux \
-                           arandr \
-                           xbacklight \
-                           lxappearance \
-                           arc-theme \
-                           breeze-icon-theme \
-                           i3 \
-                           i3blocks \
-                           feh \
-                           rofi \
-                           compton \
-                           pavucontrol \
-                           pasystray \
-                           blueman \
-                           xclip \
-                           axel \
-                           curl \
-                           zsh \
-                           tldr \
-                           htop \
-                           powerline \
-                           fonts-powerline \
-                           neofetch
+               git \
+               build-essential \
+               cmake \
+               meld \
+               valgrind \
+               tree \
+               bless \
+               openssh-server \
+               sshfs \
+               net-tools \
+               tmux \
+               arandr \
+               xbacklight \
+               lxappearance \
+               feh \
+               rofi \
+               pavucontrol \
+               pasystray \
+               blueman \
+               xclip \
+               axel \
+               curl \
+               zsh \
+               tldr \
+               htop \
+               powerline \
+               fonts-powerline \
+               neofetch \
+               chromium-browser
+
+sudo add-apt-repository --yes ppa:regolith-linux/release
+sudo apt --yes install \
+               regolith-desktop-standard \
+               regolith-look-ayu \
+               regolith-look-ayu-dark \
+               regolith-look-ayu-mirage \
+               regolith-look-cahuella \
+               regolith-look-dracula \
+               regolith-look-gruvbox \
+               regolith-look-lascaille \
+               regolith-look-nord \
+               regolith-look-pop-os \
+               regolith-look-solarized-dark \
+               regolith-look-ubuntu
 
 if [[ ! -d ~/.oh-my-zsh ]]; then
-  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
-  chsh -s $(which zsh)
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+    chsh -s $(which zsh)
 fi
 
 if [[ ! -d ~/.tmux-themepack ]]; then
-  git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
+    git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 fi
 
 if [[ ! -d /opt/piavpn/ ]]; then
-  readonly PIA_DL="pia-linux-2.1-04977.run"
-  axel "https://installers.privateinternetaccess.com/download/$PIA_DL"
-  bash "$PIA_DL"
-  sudo ln -s  /opt/piavpn/bin/pia-client /usr/local/bin/pia-client
+    readonly PIA_DL="pia-linux-2.5.1-05676.run"
+    axel "https://installers.privateinternetaccess.com/download/$PIA_DL"
+    bash "$PIA_DL"
+    sudo ln -s  /opt/piavpn/bin/pia-client /usr/local/bin/pia-client
 fi
 
 readonly FA_VERSION="5.12.1"
 if [[ ! -f fontawesome-free-${FA_VERSION}-desktop.zip ]]; then
-  mkdir -p ~/.fonts
-  axel https://github.com/FortAwesome/Font-Awesome/releases/download/${FA_VERSION}/fontawesome-free-${FA_VERSION}-desktop.zip
-  unzip -o fontawesome-free-${FA_VERSION}-desktop.zip
-  cp fontawesome-free-${FA_VERSION}-desktop/otfs/*.otf ~/.fonts/
-  axel https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
-  unzip -o YosemiteSanFranciscoFont-master.zip
-  mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/
+    mkdir -p ~/.fonts
+    axel https://github.com/FortAwesome/Font-Awesome/releases/download/${FA_VERSION}/fontawesome-free-${FA_VERSION}-desktop.zip
+    unzip -o fontawesome-free-${FA_VERSION}-desktop.zip
+    cp fontawesome-free-${FA_VERSION}-desktop/otfs/*.otf ~/.fonts/
+    axel https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
+    unzip -o YosemiteSanFranciscoFont-master.zip
+    mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/
 fi
 
 if [[ $(sudo dmidecode -s system-product-name) =~ "VirtualBox" ]]; then
-	sudo apt --yes --force-yes install virtualbox-guest-additions-iso
-	sudo adduser "$USER" vboxusers
-	sudo adduser "$USER" vboxsf
-	mkdir "/home/${USER}/share/"
-	sudo mount -t vboxsf -o uid=1000,gid=1000 vmshare "/home/${USER}/share/"
+    sudo apt --yes --force-yes install virtualbox-guest-additions-iso
+    sudo adduser "$USER" vboxusers
+    sudo adduser "$USER" vboxsf
+    mkdir "/home/${USER}/share/"
+    sudo mount -t vboxsf -o uid=1000,gid=1000 vmshare "/home/${USER}/share/"
 fi
 
 readonly SUBL_DIR="/opt/sublime_text_3/"
 if [[ ! -d "$SUBL_DIR" ]]; then
-	axel "https://download.sublimetext.com/sublime_text_3_build_3211_x64.tar.bz2"
-	tar vxjf sublime_text_3*.tar.bz2
-	sudo mv "sublime_text_3" "/opt/"
-	sudo ln -s "${SUBL_DIR}sublime_text" "/usr/bin/sb"
+    axel "https://download.sublimetext.com/sublime_text_3_build_3211_x64.tar.bz2"
+    tar vxjf sublime_text_3*.tar.bz2
+    sudo mv "sublime_text_3" "/opt/"
+    sudo ln -s "${SUBL_DIR}sublime_text" "/usr/bin/sb"
 fi
 
 readonly PYCHARM_DIR="/opt/pycharm/"
 if [[ ! -d "$PYCHARM_DIR" ]]; then
-	readonly PYCHARM_DL="pycharm-community-2020.2.1.tar.gz"
-	axel "https://download-cf.jetbrains.com/python/${PYCHARM_DL}"
-	sudo mkdir "$PYCHARM_DIR"
-	sudo tar -xzf "$PYCHARM_DL" -C "$PYCHARM_DIR" --strip-components=1
-	sudo ln -s "${PYCHARM_DIR}bin/pycharm.sh" "/usr/bin/pycharm"
+    readonly PYCHARM_DL="pycharm-professional-2020.2.3.tar.gz"
+    axel "https://download-cf.jetbrains.com/python/${PYCHARM_DL}"
+    sudo mkdir "$PYCHARM_DIR"
+    sudo tar -xzf "$PYCHARM_DL" -C "$PYCHARM_DIR" --strip-components=1
+    sudo ln -s "${PYCHARM_DIR}bin/pycharm.sh" "/usr/bin/pycharm"
 fi
 
 readonly CLION_DIR="/opt/clion/"
 if [[ ! -d "$CLION_DIR" ]]; then
-	readonly CLION_DL="CLion-2020.2.1.tar.gz"
-	axel "https://download-cf.jetbrains.com/cpp/${CLION_DL}"
-	sudo mkdir "$CLION_DIR"
-	sudo tar -xzf "$CLION_DL" -C "$CLION_DIR" --strip-components=1
-	sudo ln -s "${CLION_DIR}bin/clion.sh" "/usr/bin/clion"
+    readonly CLION_DL="CLion-2020.2.4.tar.gz"
+    axel "https://download-cf.jetbrains.com/cpp/${CLION_DL}"
+    sudo mkdir "$CLION_DIR"
+    sudo tar -xzf "$CLION_DL" -C "$CLION_DIR" --strip-components=1
+    sudo ln -s "${CLION_DIR}bin/clion.sh" "/usr/bin/clion"
 fi
 
 if [[ $(sudo dmidecode -s system-product-name) != *"VirtualBox"* ]]; then
-	curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-	sudo apt-get update && sudo apt-get install spotify-client
+    curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt-get update && sudo apt-get install spotify-client
 fi
 
 if [[ ! -d playerctl-2.1.1_amd64.deb ]]; then
