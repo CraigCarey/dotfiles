95cec79ac5d4ebc98b90834950f41657a2975671
Craig <sgccarey@gmail.com>
2020-11-30 09:14:52 +0000

    screenlayout: xps dock bugfix

--- ubuntu/.screenlayout/xps_docked.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if xrandr | grep -iq "DP-1-1 connected"; then
-    xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal
-    xrandr --output DP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-fi
