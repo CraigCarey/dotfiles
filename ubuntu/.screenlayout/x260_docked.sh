a908db147c6f5180d566c206843287377d9d0e57
Craig Carey <sgccarey@gmail.com>
2021-02-16 22:16:35 +0000

    ubuntu: some minor updates from xps

--- ubuntu/.screenlayout/x260_docked.sh
+++ ubuntu/.screenlayout/x260_docked.sh
@@ -5,7 +5,7 @@
 readonly DISPLAY="DP2-2"
 
 if xrandr | grep -q "DP2-2 connected "; then
-    xrandr --output DP2-2 --primary
+  xrandr --output DP2-2 --primary
 	xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
 	xrandr --output DP2-1 --mode 1920x1080 --pos 4480x0 --rotate normal
 	xrandr --output DP2-2 --primary --mode 2560x1440 --pos 1920x0 --rotate normal
